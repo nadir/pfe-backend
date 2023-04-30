6c97cdefb883f4cb2ca4d7e044b1c0823408feb3
Nadhir <79141224+nadir@users.noreply.github.com>
2023-04-30 23:12:13 +0100

    feat: use ssl when connecting to postgres in production

--- src/plugins/db.ts
+++ src/plugins/db.ts
@@ -4,5 +4,9 @@ import postgres, { PostgresPluginOptions } from "@fastify/postgres";
 export default fp<PostgresPluginOptions>(async (fastify) => {
     fastify.register(postgres, {
         connectionString: process.env.DATABASE_URL,
+        ssl:
+            process.env.NODE_ENV === "production"
+                ? { rejectUnauthorized: false }
+                : false,
     });
 });
