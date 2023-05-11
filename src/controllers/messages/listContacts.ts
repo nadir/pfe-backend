4b765c7658d4602b90b4fa217b8a3540b33c4654
Nadhir <79141224+nadir@users.noreply.github.com>
2023-05-11 10:32:19 +0100

    fix: fix typo in order by

--- src/controllers/messages/listContacts.ts
+++ src/controllers/messages/listContacts.ts
@@ -4,7 +4,7 @@ export const listContacts: RouteHandler = async function (req, reply) {
     //@ts-ignore
     const { userId } = req.user;
     const contacts = await this.pg.query(
-        "SELECT user_id, first_name, last_name, profile_pic FROM users WHERE user_type = $1 ORDERBY first_name ASC",
+        "SELECT user_id, first_name, last_name, profile_pic FROM users WHERE user_type = $1 ORDER BY first_name ASC",
         ["teacher"]
     );
 
